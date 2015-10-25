b34390cf1f510c9813b9c4fa81804b1b66d13f55
Skuli Arnlaugsson <skuli.arnlaugsson@gmail.com>
2015-10-25 14:05:59 +0000

    Step 5.5: Add port option and cleanup README.md
    Now it's possible to export (set) an environmental value, PORT, to override the default Spark port (4567). This can be done before running or deploying the application.

--- src/main/java/is/ru/arnlaugsson/chuck_joke/ChuckJoke.java
+++ src/main/java/is/ru/arnlaugsson/chuck_joke/ChuckJoke.java
@@ -4,8 +4,15 @@ import static spark.Spark.*;
 
 
 public class ChuckJoke {
+
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+        if (port != null) {
+            setPort(Integer.valueOf(port));
+        }
+
         Chuck chuck = new Chuck();    
         get("/random", (req, res) -> chuck.random());
     }
+
 }
