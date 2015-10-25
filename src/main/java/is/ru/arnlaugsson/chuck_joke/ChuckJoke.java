a7c1d8ea5b013b58fabe47bed36db79b5274b062
Skuli Arnlaugsson <skuli.arnlaugsson@gmail.com>
2015-10-25 13:50:23 +0000

    Step 5: Change to Spark web service
    Now bin/deploy will not show a single random quote, but start a webserver running under port 4567 on the server it's started on, and listenin to web requests to "localhost:4567/random", where it will give a new random joke for each time it is called.

--- src/main/java/is/ru/arnlaugsson/chuck_joke/ChuckJoke.java
+++ src/main/java/is/ru/arnlaugsson/chuck_joke/ChuckJoke.java
@@ -1,8 +1,11 @@
 package is.arnlaugsson.chuck_joke;
 
+import static spark.Spark.*;
+
+
 public class ChuckJoke {
     public static void main(String[] args) {
         Chuck chuck = new Chuck();    
-        System.out.println(chuck.random());
+        get("/random", (req, res) -> chuck.random());
     }
 }
